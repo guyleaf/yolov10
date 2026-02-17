fdda4f98d04926fd1e6f801174158c2ea5bc4259
Leaf Ying <leaf.ying.work@gmail.com>
2026-02-17 17:55:25 +0800

    feat: Support env setup for external pre-loading

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,12 +1,18 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
+# ruff: noqa: E402
 
 __version__ = "8.1.34"
 
+from ultralytics.utils.env import setup_environment
+
+setup_environment()
+
 from ultralytics.data.explorer.explorer import Explorer
-from ultralytics.models import RTDETR, SAM, YOLO, YOLOWorld, YOLOv10
+from ultralytics.models import RTDETR, SAM, YOLO, YOLOv10, YOLOWorld
 from ultralytics.models.fastsam import FastSAM
 from ultralytics.models.nas import NAS
-from ultralytics.utils import ASSETS, SETTINGS as settings
+from ultralytics.utils import ASSETS
+from ultralytics.utils import SETTINGS as settings
 from ultralytics.utils.checks import check_yolo as checks
 from ultralytics.utils.downloads import download
 
